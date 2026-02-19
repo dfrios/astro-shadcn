ea946fcc3fb32d6d1920b3a7c1c6ae3318751ae1
David Rios <david@davidrios.co>
2026-02-18 23:03:34 -0500

    🐛 Fix: Fixed automatic dark mode

---
+++ src/scripts/mode.ts
@@ -0,0 +1,9 @@
+const handleThemeChange = () => {
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+};
+
+handleThemeChange();
+
+const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+darkModeMediaQuery.addEventListener("change", handleThemeChange);
--- src/scripts/scripts.ts
+++
@@ -1,7 +0,0 @@
-const handleThemeChange = () => {
-  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  document.documentElement.classList[isDark ? "add" : "remove"]("dark");
-};
-
-const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-darkModeMediaQuery.addEventListener("change", handleThemeChange);
