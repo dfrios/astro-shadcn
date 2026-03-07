29523e3384b82b7b80d41cd51948c25807b50979
David Rios <david@davidrios.co>
2026-03-07 10:44:23 -0500

    ♻️ Refactor: Created a single page, not matter the language

--- src/interfaces/lang.ts
+++ src/interfaces/lang.ts
@@ -1,1 +1,13 @@
-export type Lang = 'en' | 'es';
+export type Langs = "en" | "es";
+
+export interface Lang {
+  name: string;
+  shortname: Langs;
+  img: string
+}
+
+export interface LangParams {
+  params: {
+    lang: string;
+  }
+}
