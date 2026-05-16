113cd756b0a1c0893bd7485e27aafd75390757d6
David Rios <david@davidrios.co>
2026-05-16 09:38:59 -0500

    🔧 Config: Update Prettier configuration and VSCode settings for improved formatting

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,14 +1,18 @@
 /** @type {import("prettier").Config} */
 export default {
-  printWidth: 100,
-  useTabs: true,
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-  overrides: [
-    {
-      files: ["**/*.astro"],
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+	trailingComma: "es5",
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+	printWidth: 100,
+	useTabs: true,
+	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+	overrides: [
+		{
+			files: ["**/*.astro"],
+			options: {
+				parser: "astro",
+			},
+		},
+	],
 };
