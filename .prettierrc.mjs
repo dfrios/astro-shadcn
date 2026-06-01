fc49a5df02e43b542a79bf781712405f9cd21da1
David Rios <david@davidrios.co>
2026-05-31 19:06:43 -0500

    🔧 Config: Improved Prettier configuration

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,11 +1,12 @@
 /** @type {import("prettier").Config} */
 export default {
-	trailingComma: "es5",
-	tabWidth: 2,
-	semi: false,
-	singleQuote: true,
 	printWidth: 100,
-	useTabs: true,
+	useTabs: false,
+	singleQuote: true,
+	tabWidth: 2,
+	bracketSpacing: true,
+	htmlWhitespaceSensitivity: "ignore",
+	semi: true,
 	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
 	overrides: [
 		{
@@ -16,3 +17,4 @@ export default {
 		},
 	],
 };
+
